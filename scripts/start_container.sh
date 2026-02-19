be43f4b518c85913e6ef18339b83c8da1c8be335
surajm2311 <91374884+surajm2311@users.noreply.github.com>
2026-02-19 19:54:24 +0530

    add new changes

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,5 @@ set -e
 docker pull maingadesuraj/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5001:5001 maingadesuraj/simple-python-flask-app 
+docker run -d -p 5001:5001 maingadesuraj/simple-python-flask-app
+
